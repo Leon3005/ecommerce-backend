731252a5a8e2fda98167750a72297c75f9d0b864
Leon <leonwheeler08@gmail.com>
2021-05-30 16:18:23 +0100

    Added schema for ProductTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Product,
+      key: id,
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tag,
+      key: id,
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product_tag",
+};
+
+ProductTag.init(schema, options);
 
 module.exports = ProductTag;
