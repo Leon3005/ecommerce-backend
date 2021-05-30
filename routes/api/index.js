439d0a1af3954e82b367135ffb55ba8ef498e746
Leon <leonwheeler08@gmail.com>
2021-05-30 15:55:57 +0100

    Added notes file and added schema to category class.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const { Router } = require("express");
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
+const router = Router();
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
 
 module.exports = router;
