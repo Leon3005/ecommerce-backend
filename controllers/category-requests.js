5884b079fca721e50dfc1d59174bbcefe541b356
Leon <leonwheeler08@gmail.com>
2021-06-05 17:14:24 +0100

    Moved category API functions to controllers folder.

--- controllers/category-requests.js
+++ controllers/category-requests.js
@@ -1,19 +1,97 @@
 const { Category, Product } = require("../models");
 
-const getAllCategories = async (res) =>
-  await Category.findAll({ include: { model: Product } });
+const isValid = ({ body }) => {
+  const validKeys = ["category_name"];
 
-const getCategoryById = async () => {
-  const categoryId = await Category.findByPk(req.params.id, {
-    include: { model: Product },
+  return Object.keys(body).every((keyFromReq) => {
+    return validKeys.includes(keyFromReq);
   });
-
-  if (!categoryId) {
-    res.status(404).json({ error: "No category ID found!" });
-    return;
-  }
-
-  return categoryId;
 };
 
-module.exports = { getAllCategories, getCategoryById };
+const getAllCategories = async (req, res) => {
+  try {
+    const allCategories = await Category.findAll({
+      include: { model: Product },
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getCategoryById = async (req, res) => {
+  try {
+    const categoryId = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+
+    if (!categoryId) {
+      res.status(404).json({ error: "No category ID found!" });
+      return;
+    }
+    res.status(200).json(categoryId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const newCategory = async (req, res) => {
+  try {
+    if (isValid(req)) {
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+    } else {
+      res.status(404).json({ error: "Invalid key entered!" });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updateCategory = async (req, res) => {
+  try {
+    if (isValid(req)) {
+      const updateCategory = await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!updateCategory[0]) {
+        res.status(404).json({ message: "No category with this id!" });
+        return;
+      }
+      res
+        .status(200)
+        .json({ success: "Category has been updated successfully!" });
+    } else {
+      res.status(404).json({ error: "Invalid key entered!" });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res
+      .status(200)
+      .json({ success: "Category has been deleted successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = {
+  getAllCategories,
+  getCategoryById,
+  newCategory,
+  updateCategory,
+  deleteCategory,
+};
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,99 +3,19 @@ const { Category, Product } = require("../../models");
 const {
   getAllCategories,
   getCategoryById,
+  newCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../../controllers/category-requests");
-// The `/api/categories` endpoint
 
-const isValid = ({ body }) => {
-  const validKeys = ["category_name"];
+router.get("/", getAllCategories);
 
-  return Object.keys(body).every((keyFromReq) => {
-    return validKeys.includes(keyFromReq);
-  });
-};
+router.get("/:id", getCategoryById);
 
-router.get("/", async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  try {
-    const allCategories = await getAllCategories();
-    res.status(200).json(allCategories);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/", newCategory);
 
-router.get("/:id", async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  try {
-    const categoryId = await Category.findByPk(req.params.id, {
-      include: { model: Product },
-    });
+router.put("/:id", updateCategory);
 
-    if (!categoryId) {
-      res.status(404).json({ error: "No category ID found!" });
-      return;
-    }
-    res.status(200).json(categoryId);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post("/", async (req, res) => {
-  // create a new category
-  try {
-    if (isValid(req)) {
-      const newCategory = await Category.create(req.body);
-      res.status(200).json(newCategory);
-    } else {
-      res.status(404).json({ error: "Invalid key entered!" });
-      return;
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  try {
-    if (isValid(req)) {
-      const updateCategory = await Category.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!updateCategory[0]) {
-        res.status(404).json({ message: "No category with this id!" });
-        return;
-      }
-      res
-        .status(200)
-        .json({ success: "Category has been updated successfully!" });
-    } else {
-      res.status(404).json({ error: "Invalid key entered!" });
-      return;
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  // delete a category by its `id` value
-  try {
-    const deleteCategory = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res
-      .status(200)
-      .json({ success: "Category has been deleted successfully!" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
